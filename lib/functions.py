20def4d6a761647c8b4cb8c3594ea645297a35cb
Karla Lockhart <kblockhart@gmail.com>
2024-03-02 07:44:21 -0800

    passed tests

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
 
+#def greet_programmer():
+#    pass
+
+#def greet(name):
+#    pass
+
+#def greet_with_default(name="programmer"):
+#    pass
+
+#def add(num1, num2):
+#    pass
+
+#def halve(number):
+#    pass
+
+#!/usr/bin/env python3
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
+
+
+#def my_function(param):
+#    print("Running my_function")
+#    return param + 1
+
+#my_function_return_value = my_function(1)
